6273dd8ce558e96259cc364f087043942626bfa2
malvinharis <alvinharis08@gmail.com>
2021-06-17 16:31:09 +0700

    Add About Page

---
+++ pages/about.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="main__section">
+    <div class="main__about">
+      <div class="about">
+        <div class="about__headline">
+          <span class="font-semibold">About Us</span>
+        </div>
+        <div class="about__description">
+          <p class="pb-4 font-bold text-4xl tracking-wider">
+            Created out of our passion for travel and sharing inspiring stories
+          </p>
+          <p>
+            TS Media was established to inspire, educate and entertain audiences
+            of different segments of travel, culinary, lifestyle, hobbies,
+            sports and finance.
+          </p>
+          <p>
+            By collaborating with different brands and talents, together we can
+            create quality contents and targeted digital campaigns by focusing
+            on awareness, activation and retention.
+          </p>
+        </div>
+      </div>
+      <div class="about-points">
+        <div class="about__headline">
+          <span class="font-semibold">What We Can Do</span>
+        </div>
+        <div class="about-points__list">
+          <div class="about-points__item">
+            <span class="font-bold text-4xl tracking-wider">Awareness</span>
+            <p>
+              Helping brands create brand awareness or brand recall through our
+              personalized digital contents and campaigns.
+            </p>
+          </div>
+          <div class="about-points__item">
+            <span class="font-bold text-4xl tracking-wider">Aspiration</span>
+            <p>
+              Through storytelling and our various digital channels, we help
+              brands create a market and product education to build trust and
+              aspire the audience.
+            </p>
+          </div>
+          <div class="about-points__item">
+            <span class="font-bold text-4xl tracking-wider">Activation</span>
+            <p>
+              Work with us to increase sales conversion and using our channels
+              as your lead generation platform.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="background">
+      <div class="h-full w-full">
+        <img
+          class="object-cover"
+          :src="require('~/assets/img/bg-gradient2.png')"
+          alt=""
+        />
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+<style></style>
