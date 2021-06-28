5c343ab28f9343d4a9f54aeed869ca20bc582d90
malvinharis <alvinharis08@gmail.com>
2021-06-28 18:46:52 +0700

    Optimize Responsive in About Page

--- pages/about.vue
+++ pages/about.vue
@@ -4,19 +4,19 @@
       <div class="main__about-wrapper">
         <div class="about">
           <div class="about__headline">
-            <span class="font-semibold">About Us</span>
+            <span class="text-sm font-semibold">About Us</span>
           </div>
           <div class="about__description">
-            <p class="pb-4 font-bold text-5xl tracking-wider">
+            <p class="pb-4 font-bold text-2xl lg:text-5xl tracking-wider">
               Created out of our passion for travel and sharing inspiring
               stories
             </p>
-            <p class="text-xl">
+            <p class="text-sm lg:text-xl">
               TS Media was established to inspire, educate and entertain
               audiences of different segments of travel, culinary, lifestyle,
               hobbies, sports and finance.
             </p>
-            <p class="text-xl">
+            <p class="text-sm lg:text-xl">
               By collaborating with different brands and talents, together we
               can create quality contents and targeted digital campaigns by
               focusing on awareness, activation and retention.
@@ -29,23 +29,29 @@
           </div>
           <div class="about-points__list">
             <div class="about-points__item">
-              <span class="font-bold text-4xl tracking-wider">Awareness</span>
-              <p>
+              <span class="font-bold text-2xl lg:text-4xl tracking-wider">
+                Awareness
+              </span>
+              <p class="text-sm lg:text-base">
                 Helping brands create brand awareness or brand recall through
                 our personalized digital contents and campaigns.
               </p>
             </div>
             <div class="about-points__item">
-              <span class="font-bold text-4xl tracking-wider">Aspiration</span>
-              <p>
+              <span class="font-bold text-2xl lg:text-4xl tracking-wider">
+                Aspiration
+              </span>
+              <p class="text-sm lg:text-base">
                 Through storytelling and our various digital channels, we help
                 brands create a market and product education to build trust and
                 aspire the audience.
               </p>
             </div>
             <div class="about-points__item">
-              <span class="font-bold text-4xl tracking-wider">Activation</span>
-              <p>
+              <span class="font-bold text-2xl lg:text-4xl tracking-wider">
+                Activation
+              </span>
+              <p class="text-sm lg:text-base">
                 Work with us to increase sales conversion and using our channels
                 as your lead generation platform.
               </p>
